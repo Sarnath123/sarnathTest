cec8bd6ab080d8aa0af8236d37b6677b9f5fee8d
weixiaoxiao <1002866390@qq.com>
2016-07-20 14:46:11 +0800

    test
    test together

--- Paomadeng/Paomadeng/ViewController.m
+++ Paomadeng/Paomadeng/ViewController.m
@@ -17,6 +17,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    self.title = @"新建工程";
 }
 
 - (void)didReceiveMemoryWarning {
