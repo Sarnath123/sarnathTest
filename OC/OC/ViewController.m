77445d1d7c42bea3d1684927fe083594e1df8c14
Sense <dazhivip@163.com>
2016-07-20 14:55:45 +0800

    H5测试工程

---
+++ OC/OC/ViewController.m
@@ -0,0 +1,36 @@
+//
+//  ViewController.m
+//  OC
+//
+//  Created by Sarnath RD on 16/7/18.
+//  Copyright © 2016年 Sarnath. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+@property(nonatomic,strong)UIWebView * webView;
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+	[super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+	NSURL *url = [[NSBundle mainBundle] URLForResource:@"untitled" withExtension:@"html"];
+	NSURLRequest *request = [NSURLRequest requestWithURL:url];
+	
+	self.webView = [[UIWebView alloc]init];
+	self.webView.frame = self.view.bounds;
+
+	self.webView.scrollView.scrollEnabled = NO;
+	[self.webView loadRequest:request];
+	[self.view addSubview:self.webView];
+}
+
+- (void)didReceiveMemoryWarning {
+	[super didReceiveMemoryWarning];
+	// Dispose of any resources that can be recreated.
+}
+
+@end
